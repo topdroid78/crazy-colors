86a8b4e57833c15a4bc6b786db564e4567533dc2
topdroid78 <alexnistor978@gmail.com>
2023-01-03 00:20:57 +0100

    Created the initial app

---
+++ script.js
@@ -0,0 +1,30 @@
+let body = document.querySelector('body');
+document.querySelector('#blue').addEventListener('click', function()
+{
+    body.style.backgroundColor = ('navy')
+})
+
+document.querySelector('#pink').addEventListener('click', function()
+{
+    body.style.backgroundColor = ('fuchsia')
+})
+
+document.querySelector('#orange').addEventListener('click', function()
+{
+    body.style.backgroundColor = ('orange')
+})
+
+document.querySelector('#green').addEventListener('click', function()
+{
+    body.style.backgroundColor = ('lime')
+})
+
+document.querySelector('#silver').addEventListener('click', function()
+{
+    body.style.backgroundColor = ('silver')
+})
+
+
+
+
+
